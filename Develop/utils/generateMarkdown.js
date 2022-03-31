eb9b682cdd4f66a4c6c1fe5016ce66a9737c3596
AD <archied5150@yahoo.com>
2022-03-30 23:21:53 -0500

    markdown is generated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,28 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // https://img.shields.io/badge/License-<STATUS>-<COLOR>.svg
+  //using the license selected, generate the badge
+  return `
+  ![License](https://img.shields.io/badge/License-${license}-green.svg)
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  //using the license selected, generate the link
+  if(license){
+    return `
+  this is the license link
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  // console.log(data);
+  //destructure the data
+  const {title, description, install, usage, test, license, username, email} = data;
+  
   return `
-    # ${data.title}
-    ${data.title}
-    ${data.description}
-    ${data.install}
-    ${data.usage}
-    ${data.test}
-    ${data.license}
-    ${data.username}
-    ${data.email}
+  # ${title}
+
+  ### License
+  ${renderLicenseSection(license)}
+
+  ## Description
+
+  ${description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Features](#features)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${install}
+
+  ## Usage
+
+  ${usage}
+
+  ## Tests
+
+  ${test}
+
+  ## Questions
+
+  Have questions? feel free to reach me here:
+
+  ${email}
+  https://github.com/${username}
 
 `;
 }
--- dist/readME.md
+++ dist/readME.md
@@ -1,11 +1,44 @@
 
-    # project 1
-    project 1
-    it works
-    install
-    it is used
-    tested
-    2
-    Archiedonaho
-    ad@gmail.com
+  # kalsdfjklas
+
+  ### License
+  
+  
+  ![License](https://img.shields.io/badge/License-MIT-green.svg)
+  
+  
+  this is the license link
+    
+  
+
+  ## Description
+
+  askldfljaklsd
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Features](#features)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## Installation
+
+  aklsdfjaslkd
+
+  ## Usage
+
+  akdsfjklasdjf
+
+  ## Tests
+
+  klasdjflkasdj
+
+  ## Questions
+
+  Have questions? feel free to reach me here:
+
+  aklsdjfklas
+  https://github.com/kaljdsfklsj
 
